9deb10e01dd4f10ec766a7aa3a76698c77dfe5d5
Kenmakhanu <Kennedy.makhanu@gmail.com>
2023-03-21 16:07:43 -0400

    backend

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-1"
+  region = "us-west-2"
 }
 
 resource "aws_vpc" "actions" {
@@ -8,5 +8,21 @@ resource "aws_vpc" "actions" {
   tags = {
     Name = "Class30"
     Team = "DevOps"
+  }
+}
+
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0" # Optional but recommended in production
+    }
+  }
+  backend "s3" {
+    bucket = "myotherclass25bucket"
+    key = "dev/terraform.tfstate"
+    #dynamodb_table = "terraform-lock"
+    region = "us-west-2"
   }
 }
