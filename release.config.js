21e99e729b666e0cb64a46fd77243044613355e1
Kenmakhanu <Kennedy.makhanu@gmail.com>
2023-03-21 16:50:37 -0400

    feat: versioning

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-       branches: "master",
-       repositoryUrl: "https://github.com/Kenmakhanu/actionstest.git",
+       branches: "main",
+       repositoryUrl: "hrttps://github.com/Kenmakhanu/actionstest.git",
        plugins: [
            '@semantic-release/commit-analyzer',
            '@semantic-release/release-notes-generator',
