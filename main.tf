b8b12914b47ff4a014d91b97939328fc6cead949
magamudi <118616073+magamudi@users.noreply.github.com>
2023-03-28 11:25:16 -0400

    fix: new day new me

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "magnusvpc" {
+resource "aws_vpc" "newvpc" {
    cidr_block = "10.0.0.0/24"
 
   tags = {
-    Name = "Class30"
+    Name = "trial"
     Team = "DevOps"
     Environment = "Prod"
     Division = "HR"
@@ -22,9 +22,33 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "class-magnus-amudi"
-    key = "dev/terraform.tfstate"
+    bucket = "aws_s3_bucket.onebucket.id"
+    key = "new/terraform.tfstate"
     region = "us-east-1"
   }
+}
+
+resource "aws_s3_bucket" "onebucket" {
+   bucket = "var.bucket"
+   acl = "private"
+   versioning {
+      enabled = true
+   }
+   tags = {
+     Name = "newbucket"
+     Environment = "Test"
+   }
+}
+
+resource "random_string" "random_suffix" {
+  length  = 3
+  special = false
+  upper   = false
+}
+
+variable "bucket" {
+  type = string
+  description = "this is the name of my s3 bucket"
+  default = "s3-bucket-${random_string.random_suffix.result}"
 }
 
